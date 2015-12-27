c7531f117373404a794a7b5bd50a6e0a4ed142d1
Yuya Seki <y.seki.14870@gmail.com>
2015-12-28 01:53:10 +0900

    Implement feature to remove cities

--- test_put_dots.py
+++ test_put_dots.py
@@ -21,17 +21,33 @@ class CityMap:
         self.ax.set_aspect('equal')
         self.ax.xaxis.set_major_locator(NullLocator())
         self.ax.yaxis.set_major_locator(NullLocator())
-        self.cid = self.fig.canvas.mpl_connect('button_press_event', self.put_city)
+        self.cid_putdot = self.fig.canvas.mpl_connect('button_press_event', self.put_city)
+        self.cid_remove = self.fig.canvas.mpl_connect('pick_event', self.remove_city)
         plt.show()
 
     def put_city(self, event):
         if event.inaxes != self.ax: return
+        if event.button != 1: return
         x = event.xdata
         y = event.ydata
         self.xdata.append(x)
         self.ydata.append(y)
-        self.ax.plot(x, y, 'bo', markersize=10)
+        self.ax.plot(x, y, 'bo', markersize=10, picker=5)
         plt.draw()
+
+    def remove_city(self, event):
+        mouseevent = event.mouseevent
+        if mouseevent.button != 3: return
+        thisline = event.artist
+        ind = event.ind[0]
+        del self.xdata[ind]
+        del self.ydata[ind]
+        thisline.remove()
+        plt.draw()
+
+    def disconnect(self):
+        self.fig.canvas.mpl_disconnect(self.cid_putdot)
+        self.fig.canvas.mpl_disconnect(self.cid_remove)
 
 
 if __name__ == '__main__':
