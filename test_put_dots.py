98ddf82054b382fb1f8ec91072605c5622134b1b
Yuya Seki <y.seki.14870@gmail.com>
2015-12-28 03:17:59 +0900

    Implement function to clear all cities

--- test_put_dots.py
+++ test_put_dots.py
@@ -13,15 +13,19 @@ def main():
 class CityMap:
     def __init__(self):
         self.fig = plt.figure()
+        self.init_axes()
+        self.cid_putdot = self.fig.canvas.mpl_connect('button_press_event', self.put_city)
+        self.cid_remove = self.fig.canvas.mpl_connect('pick_event', self.remove_city)
+        self.cid_clear = self.fig.canvas.mpl_connect('button_press_event', self.clear_dots)
+        plt.show()
+
+    def init_axes(self):
         self.ax = self.fig.add_subplot(111)
         self.ax.set_xlim(0.0, 1.0)
         self.ax.set_ylim(0.0, 1.0)
         self.ax.set_aspect('equal')
         self.ax.xaxis.set_major_locator(NullLocator())
         self.ax.yaxis.set_major_locator(NullLocator())
-        self.cid_putdot = self.fig.canvas.mpl_connect('button_press_event', self.put_city)
-        self.cid_remove = self.fig.canvas.mpl_connect('pick_event', self.remove_city)
-        plt.show()
 
     def put_city(self, event):
         if event.inaxes != self.ax: return
@@ -46,9 +50,17 @@ class CityMap:
         lines = self.ax.lines
         return np.array([line.get_ydata() for line in lines]).flatten()
 
+    def clear_dots(self, event):
+        if event.inaxes != self.ax: return
+        if event.button != 2: return
+        self.ax.cla()
+        self.init_axes()
+        plt.draw()
+
     def disconnect(self):
         self.fig.canvas.mpl_disconnect(self.cid_putdot)
         self.fig.canvas.mpl_disconnect(self.cid_remove)
+        self.fig.canvas.mpl_disconnect(self.cid_clear)
 
 
 if __name__ == '__main__':
