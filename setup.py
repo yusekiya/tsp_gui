2084f3be609a06e5b0f1c4083a5779ea57a1d751
Yuya Seki <y.seki.14870@gmail.com>
2016-01-13 23:35:30 +0900

    Update setup script

--- setup.py
+++ setup.py
@@ -5,19 +5,12 @@ base = None
 if sys.platform == "win32":
     base = "Win32GUI"
 
-build_exe_options = {'packages': ['matplotlib.backends.backend_qt4agg',
-                                    'scipy']}
-
-def load_scipy_patched(finder, module):
-    """the scipy module loads items within itself in a way that causes
-        problems without the entire package and a number of other subpackages
-        being present."""
-    finder.IncludePackage("scipy._lib")  # Changed include from scipy.lib to scipy._lib
-    finder.IncludePackage("scipy.misc")
-
-hooks.load_scipy = load_scipy_patched
+build_exe_options = {'excludes': ['tkinter', 'multiprocessing', 'OpenSSL',
+                                  'PIL', 'IPython', 'ConfigParser', 'configparser'
+                                  'SocketServer', 'asyncio', 'Queue',
+                                  'cffi', 'curses', 'html']}
 
 setup(name='TSPSolver',
-      version=1.1,
+      version='1.1',
       options={'build_exe': build_exe_options},
       executables=[Executable('tsp_gui.py', base=base)])
